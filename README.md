f6b39e5c83edc1e98d522dc4f6a142bde2c94804
andrew.dragunskih <andrew.dragunskih@yandex.ru>
2022-05-01 22:32:16 +0500

    add generate_diff()

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AndrewDragunskih/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/AndrewDragunskih/python-project-lvl2/actions)
+
+https://asciinema.org/a/491459
--- hexlet_python_package/gendiff.py
+++ hexlet_python_package/gendiff.py
@@ -1,19 +1,47 @@
 """Some description."""
-import argparse
+import json
+import os
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-    )
-parser.add_argument('first_file',nargs='?')
-parser.add_argument('second_file',nargs='?')
-parser.add_argument('-f', '--format', 
-    help='set format of output')
-parser.parse_args()
+def downcase_bool(value):
+    if value is True or value is False:
+        value = str(value)
+        return value.lower()
+    return value
 
 
-def main():
+def print_one_diff(key, value, arg):
+    result = '  {} {}: {}\n'.format(arg, key, downcase_bool(value))
+    return result
+
+
+def print_two_diffs(key, value1, value2):
+    result1 = print_one_diff(key, value1, '-')
+    result2 = print_one_diff(key, value2, '+')
+    return result1 + result2
+
+
+def generate_diff(first_file_path, second_file_path):
     """Some desription."""
-
-
-if __name__ == '__main__':
-    main()
+    first_file = json.load(open(os.path.abspath(first_file_path)))
+    second_file = json.load(open(os.path.abspath(second_file_path)))
+    first_file_keys = list(first_file.keys())
+    second_file_keys = list(second_file.keys())
+    unique_keys = list(set(first_file_keys + second_file_keys))
+    unique_keys_sorted = sorted(unique_keys)
+    result = '{\n'
+    for key in unique_keys_sorted:
+        if key in first_file_keys and key in second_file_keys:
+           if first_file[key] == second_file[key]:
+               result = result + print_one_diff(key, first_file[key],' ')
+               continue
+           else:
+               result = result + print_two_diffs(key, first_file[key], second_file[key])
+               continue
+        if key in first_file_keys and not (key in second_file_keys):
+           result = result + print_one_diff(key, first_file[key],'-')
+           continue
+        if not(key in first_file_keys) and key in second_file_keys:
+           result = result + print_one_diff(key, second_file[key],'+')
+           continue
+    result = result + '}'
+    return result
