2dd859c1b7174e07531eda9cc204b658cae085c9
andrew.dragunskih <andrew.dragunskih@yandex.ru>
2022-05-02 09:36:55 +0500

    temparary commit

--- hexlet_python_package/gendiff.py
+++ hexlet_python_package/gendiff.py
@@ -2,46 +2,87 @@
 import json
 import os
 
-def downcase_bool(value):
-    if value is True or value is False:
-        value = str(value)
-        return value.lower()
-    return value
+
+def downcase_bool(key_value):
+    """
+    Downcase True oe False to print.
+
+    Args:
+        key_value : value to downcase
+
+    Returns:
+        str: dowcased value
+    """
+    if key_value is True or key_value is False:
+        key_value = str(key_value)
+        return key_value.lower()
+    return key_value
 
 
-def print_one_diff(key, value, arg):
-    result = '  {} {}: {}\n'.format(arg, key, downcase_bool(value))
-    return result
+def format_one_diff(key, key_value, arg):
+    """
+    Format difference with one string.
+
+    Args:
+        key: key to print
+        key_value: value to print
+        arg: argument of difference
+
+    Returns:
+        str: formatted difference
+    """
+    downcased_key_value = downcase_bool(key_value)
+    return '  {0} {1}: {2}\n'.format(arg, key, downcased_key_value)
 
 
-def print_two_diffs(key, value1, value2):
-    result1 = print_one_diff(key, value1, '-')
-    result2 = print_one_diff(key, value2, '+')
-    return result1 + result2
+def format_two_diffs(key, key_value1, key_value2):
+    """
+    Format difference with two strings.
+
+    Args:
+        key: key to print
+        key_value1: value to print
+        key_value2: value to print
+
+    Returns:
+        str: formatted difference
+    """
+    formatted_string = format_one_diff(key, key_value1, '-')
+    formatted_string = formatted_string + format_one_diff(key, key_value2, '+')
+    return formatted_string
 
 
 def generate_diff(first_file_path, second_file_path):
-    """Some desription."""
-    first_file = json.load(open(os.path.abspath(first_file_path)))
-    second_file = json.load(open(os.path.abspath(second_file_path)))
+    """
+    Print difference between two files.
+
+    Args:
+        first_file_path: path to first file
+        second_file_path: path to second file
+
+    Returns:
+        str: formatted difference between files
+    """
+    with open(os.path.abspath(first_file_path), 'r') as read_file1:
+        first_file = json.load(read_file1)
     first_file_keys = list(first_file.keys())
+    with open(os.path.abspath(second_file_path), 'r') as read_file2:
+        second_file = json.load(read_file2)
     second_file_keys = list(second_file.keys())
-    unique_keys = list(set(first_file_keys + second_file_keys))
-    unique_keys_sorted = sorted(unique_keys)
-    result = '{\n'
+    unique_keys_sorted = sorted(list(set(first_file_keys + second_file_keys)))
+    diffs = '{\n'
     for key in unique_keys_sorted:
         if key in first_file_keys and key in second_file_keys:
-           if first_file[key] == second_file[key]:
-               result = result + print_one_diff(key, first_file[key],' ')
-               continue
-           else:
-               result = result + print_two_diffs(key, first_file[key], second_file[key])
-               continue
+            if first_file[key] == second_file[key]:
+                diffs = diffs + format_one_diff(key, first_file[key], ' ')
+                continue
+            else:
+                diffs = diffs + format_two_diffs(key, first_file[key], second_file[key])
+                continue
         if key in first_file_keys and not (key in second_file_keys):
-           result = result + print_one_diff(key, first_file[key],'-')
-           continue
-        if not(key in first_file_keys) and key in second_file_keys:
-           result = result + print_one_diff(key, second_file[key],'+')
-           continue
-    result = result + '}'
-    return result
+            diffs = diffs + format_one_diff(key, first_file[key], '-')
+            continue
+        if not (key in first_file_keys) and key in second_file_keys:
+            diffs = diffs + format_one_diff(key, second_file[key], '+')
+    diffs = '{0}\n}'.format(diffs)
+    return diffs
--- hexlet_python_package/scripts/gendiff.py
+++ hexlet_python_package/scripts/gendiff.py
@@ -1,25 +1,19 @@
 """Some description."""
 import argparse
-from hexlet_python_package.gendiff import generate_diff
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-    )
-parser.add_argument('first_file',nargs='?')
-parser.add_argument('second_file',nargs='?')
-parser.add_argument('-f', '--format', 
-    help='set format of output')
-parser.parse_args()
+from hexlet_python_package.gendiff import generate_diff
 
 
 def main():
-    """Some desription."""
-    print('File1 path (absolute):\n    /home/andrew/python-project-lvl2/file1.json')
-    print('File2 path (absolute):\n    /home/andrew/python-project-lvl2/file2.json')
-    print(generate_diff('/home/andrew/python-project-lvl2/file1.json', '/home/andrew/python-project-lvl2/file2.json'))
-    print('\nFile1 path:\n    file1.json')
-    print('File2 path:\n    file2.json')
+    """Run generate_diff."""
+    dscr = 'Compares two configuration files and shows a difference.'
+    parser = argparse.ArgumentParser(description=dscr)
+    parser.add_argument('first_file', nargs='?')
+    parser.add_argument('second_file', nargs='?')
+    parser.add_argument('-f', '--format', help='set format of output')
+    parser.parse_args()
     print(generate_diff('file1.json', 'file2.json'))
+
 
 if __name__ == '__main__':
     main()
